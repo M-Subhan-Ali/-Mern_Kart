3711b9f757b38604c2622b41e0ef2f3959aa5ce0
M-Subhan-Ali <s8289552@gmail.com>
2026-01-30 08:29:20 +0500

    autoupdate for users in app

--- components/ClientProvider.tsx
+++ components/ClientProvider.tsx
@@ -5,12 +5,48 @@ import { AppThemeProvider } from "../theme/ThemeProvider";
 import Navbar from "./Navbar";
 import { ToastContainer } from "react-toastify";
 import ReduxProvider from "../redux/Provider";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function ClientProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      "serviceWorker" in navigator
+    ) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          registration.addEventListener("updatefound", () => {
+            const newWorker = registration.installing;
+            if (newWorker) {
+              newWorker.addEventListener("statechange", () => {
+                if (
+                  newWorker.state === "installed" &&
+                  navigator.serviceWorker.controller
+                ) {
+                  toast.info("Update available! Reloading to apply...", {
+                    onClose: () => window.location.reload(),
+                    autoClose: 3000,
+                  });
+                  setTimeout(() => {
+                    window.location.reload();
+                  }, 3500);
+                }
+              });
+            }
+          });
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+  }, []);
+
   return (
     <ReduxProvider>
       <CookiesProvider>
